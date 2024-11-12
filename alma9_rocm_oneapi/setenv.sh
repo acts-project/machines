d1d6061d3325840f11909ab9866963bc7796479b
flg <fabrice@legoff.eu>
2024-11-12 12:44:30 +0100

    added container to build traccc/SYCL for AMD GPU backend on Alma9 (#126)

---
+++ alma9_rocm_oneapi/setenv.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+GCCDIR=/opt/rh/gcc-toolset-13/root
+
+# Set up gcc 13.
+source ${GCCDIR}/../enable
+# Set up SYCL compiler, including AMD backend.
+source /opt/intel/oneapi/setvars.sh --include-intel-llvm
+
+# Set up the compilers to use (with CMake).
+export CC="`which clang` --gcc-toolchain=${GCCDIR}"
+export CXX="`which clang++` --gcc-toolchain=${GCCDIR}"
+
+# Set up the compiler and its options for SYCL
+# gfx90a is the GPU at RAL
+export SYCLCXX="${CXX} -fsycl"
+export SYCLFLAGS="-fsycl-targets=amd_gpu_gfx90a -Xclang -opaque-pointers"
+
+# HIP_VISIBLE_DEVICES=all does not work
+export HIP_VISIBLE_DEVICES=0,1
+
+# Clean
+unset GCCDIR
+
+# For user's convenience:
+export TRACCC_URL="https://github.com/acts-project/traccc"
+echo "git clone $TRACCC_URL"
+echo "cmake -S . -B buildamd -DTRACCC_BUILD_SYCL=ON -DTRACCC_USE_ROOT=OFF -DCMAKE_INSTALL_PREFIX:PATH=./installed"
+echo "cmake --build buildamd --target install"
+
+bash
